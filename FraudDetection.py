2a96b642268cee7c0fd0d50daa9c503bd2f5c79a
markoo26 <markpyt1992@gmail.com>
2019-08-23 20:51:39 +0200

    'Normal' variables, NULL-count and corrmat attempt

--- FraudDetection.py
+++ FraudDetection.py
@@ -11,6 +11,9 @@ train_id = pd.read_csv("train_identity.csv")
 test_id = pd.read_csv("test_identity.csv")
 end = time.time()
 
+y_train = train_tran["isFraud"]
+train_tran = train_tran.drop(columns ="isFraud")
+
 print(end-start) ## Execution time - 87 seconds
 
 def pd_array_columns(pd_array):
@@ -33,6 +36,26 @@ train_tran["TransactionAmt"] = np.log1p(train_tran["TransactionAmt"])
 
 sns.distplot(train_tran["TransactionAmt"])
 
-all_data_na = (sum(train_tran.isnull()) / len(train_tran))
+all_data_na = train_tran.isnull().sum() / len(train_tran) * 100
+all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending = True)[:30]
 
-train_tran.fillna(False).select_dtypes(include['bool']).sum(axis=1)
+all_data_nv = train_tran[normal_variables].isnull().sum() / train_tran.shape[0] * 100
+
+f, ax = plt.subplots(figsize=(15,12))
+plt.xticks(rotation = 90)
+sns.barplot(all_data_nv.index, all_data_nv)
+plt.xlabel('Variables')
+plt.ylabel('Percentage of missing values')
+plt.title('Potentially best variables')
+
+normal_variables = []
+
+    for i in range(1, 17):
+        normal_variables.append(train_tran.columns[i-1])
+
+train_tran_nv = train_tran[normal_variables] ### NULL values need to be handled
+corrmat = train_tran_nv.corr()
+plt.subplots(figsize=(12,9))
+sns.heatmap(train_tran_nv, square = True, annot = True)
+
+corrmat.pivot_table()
