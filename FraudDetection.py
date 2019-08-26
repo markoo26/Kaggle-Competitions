f61c888ea492cb3e38e3c53cc374d1866c432a62
markoo26 <markpyt1992@gmail.com>
2019-08-26 21:48:10 +0200

    Raw Train / Train_Test_Split

--- FraudDetection.py
+++ FraudDetection.py
@@ -4,9 +4,12 @@ import pandas as pd, numpy as np, matplotlib.pyplot as plt, seaborn as sns
 import os, time, webbrowser
 
 from scipy import stats
+from scipy.stats import norm, skew, poisson
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, roc_curve, auc
+from sklearn.model_selection import train_test_split
+
 
 ### Set working directory
 
@@ -26,14 +29,20 @@ def pd_array_columns(pd_array):
 ### Data import & preparation
 
 start = time.time()
+raw_train =  pd.read_csv("train_transaction.csv")
 train_tran = pd.read_csv("train_transaction.csv")
 test_tran = pd.read_csv("test_transaction.csv")
 train_id = pd.read_csv("train_identity.csv")
 test_id = pd.read_csv("test_identity.csv")
-end = time.time()
 
-y_train = train_tran["isFraud"]
-train_tran = train_tran.drop(columns ="isFraud")
+full_train_data = train_tran.merge(train_id, on="TransactionID") #!#!# Znaczne ograniczenie zbioru danych przez inner joina
+full_test_data = test_tran.merge(test_id, on="TransactionID")
+
+X_train, X_test, y_train, y_test = train_test_split(full_train_data.drop("isFraud",1),
+                                                    full_train_data["isFraud"],
+                                                    test_size = .2, random_state = 0)
+
+end = time.time()
 
 print(end-start) ## Execution time - 87 seconds        
 
@@ -91,19 +100,14 @@ ed_summary = train_tran.groupby(by = "P_emaildomain").count().iloc[:,1]
 train_tran_nv = train_tran[normal_variables] ### NULL values need to be handled
 corrmat = train_tran_nv.corr()
 
-plt.subplots(figsize=(12,9))
-sns.heatmap(train_tran_nv, square = True, annot = True)
+#plt.subplots(figsize=(12,9))
+#sns.heatmap(train_tran_nv, square = True, annot = True)
+#corrmat.pivot_table()
 
-corrmat.pivot_table()
-
-train_tran[normal_variables].columns
-
-
-pd.DataFrame([train_tran["TransactionAmt"], y_train]).corr()
+### Correlation between one variable and train #!#!# Czy tu w ogole jest sens liczyc korelacje ze zmienna 01?
 
 
-
-test_tran = test_tran[notnull_columns]
+raw_train["isFraud","TransactionAmt"].corr()
 sc_X.fit_transform(test_tran)
 
 train_notnull = train_stan.isnull().sum()==0
