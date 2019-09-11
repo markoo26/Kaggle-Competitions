17927873aded055a74d89d6e76fdbda8368fb17c
markoo26 <markpyt1992@gmail.com>
2019-09-11 23:20:35 +0200

    Update of Train_Numeric

--- FraudDetection.py
+++ FraudDetection.py
@@ -10,7 +10,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, roc_curve, auc
 from sklearn.model_selection import train_test_split
 
-
 ### Set working directory
 
 os.chdir("C:\\Users\\Marek\\Desktop\\Python\\Kaggle\\FraudDetection")
@@ -29,7 +28,7 @@ def pd_array_columns(pd_array):
 ### Data import & preparation
 
 start = time.time()
-raw_train =  pd.read_csv("train_transaction.csv")
+#raw_train =  pd.read_csv("train_transaction.csv")
 train_tran = pd.read_csv("train_transaction.csv")
 test_tran = pd.read_csv("test_transaction.csv")
 train_id = pd.read_csv("train_identity.csv")
@@ -38,40 +37,52 @@ test_id = pd.read_csv("test_identity.csv")
 full_train_data = train_tran.merge(train_id, on="TransactionID") #!#!# Znaczne ograniczenie zbioru danych przez inner joina
 full_test_data = test_tran.merge(test_id, on="TransactionID")
 
+### Split into training and test set 
+
 X_train, X_test, y_train, y_test = train_test_split(full_train_data.drop("isFraud",1),
                                                     full_train_data["isFraud"],
                                                     test_size = .2, random_state = 0)
 
-end = time.time()
+av_columns = list(full_train_data.columns.values)
 
+end = time.time()
 print(end-start) ## Execution time - 87 seconds        
 
-### All columns & their indices
+### All columns & their indexes
 
-pd_array_columns(train_tran)
+pd_array_columns(X_train)
 
 ### Assignment to 'normal variables' (all with meaningful variable name)
 
-normal_variables = []
+mngful_variables = []
 
 for i in range(1, 17):
-    normal_variables.append(train_tran.columns[i-1])
+    mngful_variables.append(X_train.columns[i-1])
+
+for i in range(431,433):
+    mngful_variables.append(X_train.columns[i])
 
 ### Identifying and standardizing numeric (int/float64) features
 
-train_numeric = train_tran.dtypes.isin(["float64","int64"])
-train_numeric = train_numeric[train_numeric==1.0]
+train_numeric = X_train.dtypes.isin([np.float64,np.int64])
+
+train_numeric = X_train.select_dtypes(include=['float64', 'int64']).columns.tolist()
+numeric_indexes = [int(X_train.columns.get_loc(c)) for c in train_numeric if c in X_train.columns]
+
 
 sc_X = StandardScaler()
-sc_X.fit_transform(train_tran[train_numeric.index])
+sc_X.fit_transform(X_train[:,numeric_indexes])
 
+]
 ### Outliers ###
 
 fig,ax = plt.subplots()
-ax.scatter(train_tran["TransactionAmt"], train_tran["card1"])
+ax.scatter(x= X_train["ProductCD"], y= y_train)
 plt.ylabel("Transaction Amount")
 plt.xlabel("Card1 Amount")
 plt.show()
+
+### Outliers 
 
 ### Log transformation of TransactionAmt #!#!# Check if all variables need that, loop with skewed vars
 
@@ -80,9 +91,11 @@ sns.distplot(train_tran["TransactionAmt"])
 
 ### Summary of NULL values
 
-all_data_na = train_tran.isnull().sum() / len(train_tran) * 100
+all_data_na = X_train.isnull().sum() / len(X_train) * 100
 all_data_no_null = all_data_na.drop(all_data_na[all_data_na != 0].index).sort_values(ascending = True)[:30]
-all_data_nv = train_tran[normal_variables].isnull().sum() / train_tran.shape[0] * 100
+all_data_nv = X_train[numeric_indexes].isnull().sum() / train_tran.shape[0] * 100
+
+X_train[mngful_variables]
 
 f, ax = plt.subplots(figsize=(15,12))
 plt.xticks(rotation = 90)
@@ -93,8 +106,8 @@ plt.title('Potentially best variables')
 
 ### Summary of categorical variables
 
-pc_summary = train_tran.groupby(by = "ProductCD").count().iloc[:,1]
-ed_summary = train_tran.groupby(by = "P_emaildomain").count().iloc[:,1]
+pc_summary = X_train.groupby(by = "ProductCD").count().iloc[:,1]
+ed_summary = X_train.groupby(by = "P_emaildomain").count().iloc[:,1]
 
 
 train_tran_nv = train_tran[normal_variables] ### NULL values need to be handled
